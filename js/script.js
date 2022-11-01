b9ec345f3aed9f8a88a5ff402ffdf2632515b3b6
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2022-11-01 11:03:38 +0400

    add task files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// 3
+/*  
+    let n = prompt();
+    let count = 0;
+    while ( n != 0) {
+        n = Math.floor(n / 10);
+        ++count;
+    }
+    console.log("Reqemlarin sayi : "+ count);
+ */
+
+// 1
+/*   let a = prompt();
+  let flag = "sade";
+  if(a % 2 == 0 && a != 2 || a == 1){
+      flag = "murekkeb";
+  }
+  for(let i = 2; i < a / 2; i++){
+      if(a % i == 0){
+          flag = "murekkeb";
+          break;    
+      }
+  }
+ console.log(flag);
+
+ */
+
+//2 
+
+/* let n = prompt();
+
+for(let i = 1; i < n; i++ ) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i);
+    }
+}
+ */
+
+
